b8ee4a0cf2368b1a155a35e2183630a47cf2fa79
Gengachris <gengachris24@gmail.com>
2026-07-07 16:29:13 +0200

    adding 8Kyu folder.

---
+++ 7Kyu/partialWordSearching.js
@@ -0,0 +1,5 @@
+function wordSearch(query, seq){ 
+const result = seq.filter(elem => elem.toLowerCase().includes(query.toLowerCase()))
+return result.length > 0 ? result : ["Empty"];
+}
+console.log(wordSearch('muh',['chris','christopher','christine','muhizi']));
