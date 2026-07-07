9dbba2f11d09bfcb08ebed65010a491f62adb30c
Gengachris <gengachris24@gmail.com>
2026-07-07 16:37:29 +0200

    adding 6Kyu folder.

---
+++ 6Kyu/doesMyNumberLookBigInThis.js
@@ -0,0 +1,9 @@
+function narcissistic(value){
+    let strNum = value.toString();
+    let arrNum = strNum.split('');
+    // let result = arrNum.reduce((a,b) => a * b,1);
+    let result = arrNum.map( curr => curr ** strNum.length).reduce((acc,val) => acc + val,0);
+    return result === value ? true:false;
+   
+}
+console.log(narcissistic("chris"));
